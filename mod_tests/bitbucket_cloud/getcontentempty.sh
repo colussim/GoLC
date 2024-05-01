05a172423502a6fdf7f2138658f64e4285b188e6
colussim <mcolussi@bluewin.ch>
2024-05-01 06:37:11 +0200

    update

--- mod_tests/bitbucket_cloud/getcontentempty.sh
+++
@@ -1,5 +0,0 @@
-curl --header 'Authorization: Bearer ATCTT3xFfGN0SyCALJHM63oqlXa0p6njf1jUHrI5wA3lKb27F_gbd2EKYWfKPbqHYWdO-_GC7zEeHS6OWsIjOueiIFidTY7zbOPIDZ2W6VvUJck7Y0i_T0m9s1Xh--zLI9_2kOfRlbfkQodlpqlt1rw9ww7Ty9FKOkBR9Yqn9A3gGB4kJJiMUAA=F81077F0' --request GET --url 'https://api.bitbucket.org/2.0/repositories/sonarsource/test2empty/src/main/'
-
-
-#https://api.bitbucket.org/2.0/repositories/{workspace}/{repo_slug}/src/{branch}
-#'https://api.bitbucket.org/2.0/repositories/sonarsource?q=project.key="TES2"'
