1de74247954060cec683aafbf327cb9195a5414d
Emmanuel COLUSSI <111289305+emmanuel-colussi-sonarsource@users.noreply.github.com>
2024-06-18 08:02:33 +0200

    Delete scan

--- scan.sh
+++
@@ -1,9 +0,0 @@
-/Users/emmanuel.colussi/Documents/App/sonar/sonar-scanner-5.0.1.3006-macosx/bin/sonar-scanner \
-  -Dsonar.projectKey=GoLC \
-  -Dsonar.sources=. \
- -Dsonar.scm.provider=git \
-  -Dsonar.branch.name=ver1.0.3 \
--Dsonar.go.coverage.reportPaths=coverage.out \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=squ_156226eb038153d9971e047fcbb36cba6438936f\
-  -Dsonar.exclusions=config.json,dist,Release,Saves,Results,config_sample.json,createrelease.sh,imgs,.git
